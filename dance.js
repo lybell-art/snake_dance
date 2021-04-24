3c3d25196f5182094388495265eaa0a944486c27
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-24 20:50:41 +0900

    Update dance.js

--- dance.js
+++ dance.js
@@ -82,8 +82,8 @@ class lybellP5Camera{
 	screenTo3D(x, y, depth=1)
 	{
 		const AxisZ=p5.Vector.sub(this.pos, this.target).normalize();
-		const AxisX=p5.Vector.cross(AxisZ, createVector(0,1,0)).normalize();
-		const AxisY=p5.Vector.cross(AxisZ, AxisX).normalize();
+		const AxisX=p5.Vector.cross(createVector(0,1,0), AxisZ).normalize();
+		const AxisY=p5.Vector.cross(AxisX, AxisZ).normalize();
 		const baseLen=this.camera.defaultEyeZ;
 		let baseO=p5.Vector.add(this.pos, p5.Vector.mult(AxisZ, -baseLen*depth));
 		baseO.add(p5.Vector.mult(AxisX, x*depth));
