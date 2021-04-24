58df8201c8095b5a472f4a2b12ea8c67f65278d0
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-24 18:14:51 +0900

    Update dance.js

--- dance.js
+++ dance.js
@@ -13,7 +13,7 @@ class snakeSegment{
 	{
 		push();
 		translate(this.pos);
-		rotateY(PI/3);
+		rotateY(PI/5);
 		rotateX(PI/12);
 		translate(0,-snakeSegment.length / 2.0,0);
 		cylinder(snakeSegment.radius, snakeSegment.length);
@@ -61,7 +61,7 @@ class lybellP5Camera{
 		
 		let y1=y*cosY - r*sinY;
 		let z1=y*sinY + r*cosY;
-		if(z * z1 < 0) return;
+//		if(z * z1 < 0) return;
 		
 		let sinX=sinX1 * cosX2 + cosX1 * sinX2;
 		let cosX=cosX1 * cosX2 - sinX1 * sinX2;
