e567669b179dc484a1f738d72c5f7785226c3864
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-24 22:02:30 +0900

    Update dance.js

--- dance.js
+++ dance.js
@@ -14,8 +14,8 @@ class snakeSegment{
 		push();
 		translate(this.pos);
 		this.dir.x=v;
-		rotateY(PI/8);
-		rotateX(PI/2);
+		rotateY( Math.atan(this.dir.x / this.dir.z) );
+		rotateX( Math.acos(-this.dir.y / this.dir.mag()) );
 		translate(0,-snakeSegment.length / 2.0,0);
 		cylinder(snakeSegment.radius, snakeSegment.length);
 		pop();
