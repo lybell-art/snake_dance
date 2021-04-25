d6dae9ffd821c2ac5c746657607536264c0b8750
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-25 14:44:55 +0900

    Update dance.js

--- dance.js
+++ dance.js
@@ -143,9 +143,9 @@ class lybellP5Camera{
 function musicRevolve(frame)
 {
 	let t=frame*PI/360;
-	let r=300+70*sin(slider1.value()*t);
+	let r=250+75*sin(slider1.value()*t);
 	let angle=t;
-	let yy=-500+80*cos(slider2.value()*t);
+	let yy=-350+80*cos(slider2.value()*t);
 	let xx=r*sin(angle);
 	let zz=r*cos(angle);
 	return new p5.Vector(xx,yy,zz);
