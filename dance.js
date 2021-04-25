d3c38a976913b8b6f7cbab6932ee587493775299
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-25 13:37:32 +0900

    Update dance.js

--- dance.js
+++ dance.js
@@ -176,9 +176,9 @@ function draw()
 	specularColor(255, 0, 0);
 	directionalLight(255, 0, 0, 0, 0, 1);
 	specularColor(0, 255, 0);
-	directionalLight(0, 255, 0, 0, 0, -1);
-//	specularColor(255, 255, 0);
-//	directionalLight(128, 128, 0, sin(PI*2/3), 0, cos(PI*2/3));
+	directionalLight(0, 255, 0, sin(PI/3), 0, cos(PI/3));
+	specularColor(255, 255, 0);
+	directionalLight(128, 128, 0, sin(PI*2/3), 0, cos(PI*2/3));
 	specularMaterial(255);
 	let mousePos=myCam.screenTo3D(mouseX - windowWidth/2,mouseY - windowHeight/2,0.4);
 	cobra.followSegment(mousePos);
